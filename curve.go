54b51463ad8d4dda8e782806156add07ed5cd17c
stkirchberg <s.t.kirchberg@web.de>
2026-02-14 19:11:46 +0100

    better curve:
    Fehler meiner Curve25519:
    - math/big ist nicht Constant-Time
     -> crypto/internal/nistec (oder selbst schreiben)
    - fehlende Validierung von Low-Order Points
    - Little-Endian Handling und Clamping
    - Garbage Collector Leaks
     -> mit Nullen überschreiben

--- curve.go
+++ curve.go
@@ -2,123 +2,143 @@ package main
 
 import (
 	"crypto/rand"
-	"math/big"
+	"crypto/subtle"
+	"encoding/binary"
+	"errors"
 )
 
-var (
-	// P = 2^255 - 19
-	curveP, _ = new(big.Int).SetString("7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffed", 16)
-	// A24 = (A - 2) / 4 = (486662 - 2) / 4 = 121665
-	curveA24 = big.NewInt(121665)
-)
+type fe [5]uint64
 
-func X25519(scalar, basePoint [32]byte) [32]byte {
-
-	sBytes := scalar
-	sBytes[0] &= 248
-	sBytes[31] &= 127
-	sBytes[31] |= 64
-	s := leBytesToBigInt(sBytes[:])
-
-	u := leBytesToBigInt(basePoint[:])
-
-	x1 := u
-	x2 := big.NewInt(1)
-	z2 := big.NewInt(0)
-	x3 := u
-	z3 := big.NewInt(1)
-
-	for i := 254; i >= 0; i-- {
-		bit := s.Bit(i)
-
-		if bit == 1 {
-			x2, x3 = x3, x2
-			z2, z3 = z3, z2
-		}
-
-		tA := new(big.Int).Add(x2, z2)
-		tA.Mod(tA, curveP)
-		aa := new(big.Int).Mul(tA, tA)
-		aa.Mod(aa, curveP)
-
-		tB := new(big.Int).Sub(x2, z2)
-		tB.Mod(tB, curveP)
-		bb := new(big.Int).Mul(tB, tB)
-		bb.Mod(bb, curveP)
-
-		e := new(big.Int).Sub(aa, bb)
-		e.Mod(e, curveP)
-
-		tC := new(big.Int).Add(x3, z3)
-		tC.Mod(tC, curveP)
-
-		tD := new(big.Int).Sub(x3, z3)
-		tD.Mod(tD, curveP)
-
-		da := new(big.Int).Mul(tD, tA)
-		da.Mod(da, curveP)
-
-		cb := new(big.Int).Mul(tC, tB)
-		cb.Mod(cb, curveP)
-
-		// Update x3, z3
-		x3 = new(big.Int).Add(da, cb)
-		x3.Mul(x3, x3)
-		x3.Mod(x3, curveP)
-
-		z3 = new(big.Int).Sub(da, cb)
-		z3.Mul(z3, z3)
-		z3.Mul(z3, x1)
-		z3.Mod(z3, curveP)
-
-		// Update x2, z2
-		x2 = new(big.Int).Mul(aa, bb)
-		x2.Mod(x2, curveP)
-
-		tmpZ := new(big.Int).Mul(e, curveA24)
-		tmpZ.Add(tmpZ, aa)
-		tmpZ.Mul(tmpZ, e)
-		z2.Mod(tmpZ, curveP)
-
-		if bit == 1 {
-			x2, x3 = x3, x2
-			z2, z3 = z3, z2
-		}
+func cswap(choice uint64, a, b *fe) {
+	mask := -(choice)
+	for i := 0; i < 5; i++ {
+		t := mask & (a[i] ^ b[i])
+		a[i] ^= t
+		b[i] ^= t
 	}
-
-	// x / z mod P
-	res := new(big.Int).Mul(x2, z2.ModInverse(z2, curveP))
-	res.Mod(res, curveP)
-
-	return bigIntToLeBytes(res)
 }
 
-// new KeyPair
-func GenerateKeyPair() (priv, pub [32]byte) {
-	rand.Read(priv[:])
+func add(out, a, b *fe) {
+	for i := 0; i < 5; i++ {
+		out[i] = a[i] + b[i]
+	}
+}
 
-	// G = 9
+func sub(out, a, b *fe) {
+	for i := 0; i < 5; i++ {
+		out[i] = (a[i] + 0x7ffffffffffffed) - b[i]
+	}
+}
+
+func mul(out, a, b *fe) {
+	var t [10]uint64
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			t[i+j] += a[i] * b[j]
+		}
+	}
+	out[0] = t[0] + 38*t[5]
+	out[1] = t[1] + 38*t[6]
+	out[2] = t[2] + 38*t[7]
+	out[3] = t[3] + 38*t[8]
+	out[4] = t[4] + 38*t[9]
+}
+
+func invert(out, in *fe) {
+	t := *in
+	for i := 0; i < 254; i++ {
+		mul(&t, &t, &t)
+	}
+	*out = t
+}
+
+func X25519(scalar, basePoint [32]byte) ([32]byte, error) {
+	s := scalar
+	s[0] &= 248
+	s[31] &= 127
+	s[31] |= 64
+
+	// Memory-Zeroing
+	defer func() {
+		for i := range s {
+			s[i] = 0
+		}
+	}()
+
+	x1 := decodeFE(basePoint)
+	x2, z2 := fe{1}, fe{0}
+	x3, z3 := x1, fe{1}
+	a24 := fe{121665}
+
+	for i := 254; i >= 0; i-- {
+		bit := uint64((s[i/8] >> (i % 8)) & 1)
+		cswap(bit, &x2, &x3)
+		cswap(bit, &z2, &z3)
+
+		var a, aa, b, bb, e, c, d, da, cb fe
+		add(&a, &x2, &z2)
+		mul(&aa, &a, &a)
+		sub(&b, &x2, &z2)
+		mul(&bb, &b, &b)
+		sub(&e, &aa, &bb)
+		add(&c, &x3, &z3)
+		sub(&d, &x3, &z3)
+		mul(&da, &d, &a)
+		mul(&cb, &c, &b)
+
+		add(&x3, &da, &cb)
+		mul(&x3, &x3, &x3)
+		sub(&z3, &da, &cb)
+		mul(&z3, &z3, &z3)
+		mul(&z3, &z3, &x1)
+
+		mul(&x2, &aa, &bb)
+		mul(&z2, &e, &a24)
+		add(&z2, &z2, &aa)
+		mul(&z2, &z2, &e)
+
+		cswap(bit, &x2, &x3)
+		cswap(bit, &z2, &z3)
+	}
+
+	var invZ fe
+	invert(&invZ, &z2)
+	mul(&x2, &x2, &invZ)
+	res := encodeFE(x2)
+
+	// Low-Order Check
+	var zero [32]byte
+	if subtle.ConstantTimeCompare(res[:], zero[:]) == 1 {
+		return [32]byte{}, errors.New("low-order point")
+	}
+	return res, nil
+}
+
+func GenerateKeyPair() (priv, pub [32]byte) {
+	if _, err := rand.Read(priv[:]); err != nil {
+		panic(err)
+	}
 	var base [32]byte
 	base[0] = 9
-
-	pub = X25519(priv, base)
+	pub, _ = X25519(priv, base)
 	return priv, pub
 }
 
-// Little-Endian Convert
-func leBytesToBigInt(b []byte) *big.Int {
-	rev := make([]byte, 32)
-	for i := 0; i < 32; i++ {
-		rev[i] = b[31-i]
+func decodeFE(in [32]byte) fe {
+	return fe{
+		binary.LittleEndian.Uint64(in[0:8]) & 0x7ffffffffffff,
+		(binary.LittleEndian.Uint64(in[6:14]) >> 3) & 0x7ffffffffffff,
+		(binary.LittleEndian.Uint64(in[12:20]) >> 6) & 0x7ffffffffffff,
+		(binary.LittleEndian.Uint64(in[19:27]) >> 1) & 0x7ffffffffffff,
+		(binary.LittleEndian.Uint64(in[25:32]) >> 4) & 0x7ffffffffffff,
 	}
-	return new(big.Int).SetBytes(rev)
 }
 
-func bigIntToLeBytes(n *big.Int) [32]byte {
-	buf := n.Bytes()
-	var res [32]byte
-	for i := 0; i < len(buf) && i < 32; i++ {
-		res[i] = buf[len(buf)-1-i]
-	}
-	return res
+func encodeFE(in fe) [32]byte {
+	var out [32]byte
+	binary.LittleEndian.PutUint64(out[0:8], in[0]|(in[1]<<51))
+	binary.LittleEndian.PutUint64(out[8:16], (in[1]>>13)|(in[2]<<38))
+	binary.LittleEndian.PutUint64(out[16:24], (in[2]>>26)|(in[3]<<25))
+	binary.LittleEndian.PutUint64(out[24:32], (in[3]>>39)|(in[4]<<12))
+	return out
 }
