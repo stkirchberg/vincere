3f27d505726ecf5c6959c5757edb8250149ebffc
stkirchberg <s.t.kirchberg@web.de>
2026-02-20 13:33:24 +0100

    #7 fixed

--- main.go
+++ main.go
@@ -167,7 +167,7 @@ func main() {
 					mu.RUnlock()
 					if exist {
 						shared, _ := X25519(currentUser.PrivKey, sender.PubKey)
-						reversed[i].Sender = "[for you] " + m.Sender
+						reversed[i].Sender = m.Sender
 						reversed[i].Content = decrypt(shared[:], m.Content)
 					}
 				}
